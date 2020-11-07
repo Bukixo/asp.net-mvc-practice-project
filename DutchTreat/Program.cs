8546e1bd8dad5d91e2087737d46d5a4817fbc57b
bukola.thompson <bukola.thompson@outlook.com>
2020-11-07 20:25:10 +0000

    created and configured database

--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -18,9 +18,18 @@ namespace DutchTreat
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void SetupConfiguration(HostBuilderContext context, IConfigurationBuilder builder)
+        {
+            //Removing the default configuration options
+            builder.Sources.Clear();
+            builder.AddJsonFile("config.json", false, true)
+                .AddEnvironmentVariables();
+        }
     }
 }
