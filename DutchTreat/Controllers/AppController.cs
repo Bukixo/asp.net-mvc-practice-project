c773016940c9684352606e77e3a4bef63e96c637
bukola.thompson <bukola.thompson@outlook.com>
2020-11-07 19:05:50 +0000

    inital commit

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.ViewModels;
+using DutchTreat.Services;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("Send Mail to");
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        [HttpGet("About")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+
+    }
+}
---
+++ DutchTreat/Services/NullMailService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Services
+{
+    public class NullMailService : IMailService
+    {
+        private ILogger<NullMailService> _logger;
+
+        public NullMailService(ILogger<NullMailService> logger)
+        {
+            _logger = logger;
+        }
+        public void SendMessage(string to)
+        {
+            //Log the message
+            _logger.LogInformation("Sent message to Buki");
+        }
+    }
+}
