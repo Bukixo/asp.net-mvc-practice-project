e1185d5a0b598ee5791759353295fabcf1cdccd4
bukola.thompson <bukola.thompson@outlook.com>
2021-01-04 13:20:54 +0000

    git add

---
+++ DutchTreat/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller")]
+    public class OrdersController
+    {
+        private IDutchRepository _repository;
+        private ILogger<OrdersController> _logger;
+
+        public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return _repository.GetAllOrders();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get orders: {ex}");
+                return BadRequestResult;
+            }
+        }
+
+    }
+}
